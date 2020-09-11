6ae2d41963fd868b00944064aa9857b6640fd8bd
Ujjwal3009 <ujjwal3009.iimt@gmail.com>
2020-09-11 13:10:24 +0530

    Homepage and Signup Page Created

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,6 +1,82 @@
-public class HomePage {
-    public static void main(String[] args) {
+/*
+ * Copyright (c) 2020,  Ujjwal. All rights reserved.
+ * UJJWAL PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 
-        
+
+import java.util.Scanner;
+
+public class HomePage {
+    String email;
+    String password;
+
+
+    void loginMessage(){
+        System.out.println("=====================Welcome To The Shopping Mall========================");
+
+
+    }
+    void loginOptions(){
+        Scanner loginOption = new Scanner(System.in);
+
+        System.out.println("1. Login\n2. Signup");
+        System.out.println("Enter your Option");
+        int option = loginOption.nextInt();
+        switch (option){
+            case 1:
+                System.out.println("===================== Enter Your Details ====================== ");
+                Signup signupDetails = new Signup();
+                signupDetails.enterSignUpDetails();
+                break;
+            case 2:
+                System.out.println("======================== Please register yourself ==============");
+                break;
+            default:
+                System.out.println("=======================Please enter Some Value ==================");
+                break;
+
+
+        }
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        HomePage homeScreen = new HomePage();
+        homeScreen.loginMessage();
+        homeScreen.loginOptions();
+
+
+
+
+
+
+
+
+
+
     }
 }
--- src/Signup.java
+++ src/Signup.java
@@ -1,3 +1,52 @@
+import java.util.Scanner;
+
 public class Signup {
+    String email;
+    String createpwd;
+    String confmpwd;
+
+    void enterSignUpDetails(){
+         Scanner enterdetails = new Scanner(System.in);
+         System.out.println("Your email id : ");
+         email = enterdetails.nextLine();
+         System.out.println("Create A password for you : ");
+         createpwd = enterdetails.nextLine();
+         System.out.println("Confirm password");
+
+         confmpwd = enterdetails.nextLine();
+
+         if(confmpwd.equals(createpwd)){
+             System.out.println("==========Please Confirm your Details=============");
+             System.out.println(email + "\n" + createpwd + "\n"+ confmpwd);
+             confirm();
+         }
+         else{
+             System.out.println("Password Mismatch \n Please Re Enter the Details ");
+             enterSignUpDetails();
+         }
+
+
+
+
+
+    }
+    void confirm(){
+        System.out.println("================= Do you Want to edit or Confirm the Details  ================= \n 1. Confirm \n 2. Edit");
+        Scanner confirmAsk = new Scanner(System.in);
+        int ask = confirmAsk.nextInt();
+        switch (ask){
+            case 1:
+                System.out.println("Thanx for registering yourself");
+                break;
+            case 2:
+                System.out.println("Please Edit your Details");
+                enterSignUpDetails();
+        }
+    }
+    public static void main(String[] args) {
+
+
+
+    }
 
 }
